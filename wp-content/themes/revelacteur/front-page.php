e71b9b28b8c09bffcdee4ba49e53f4d4db01a109
Fanfaneee <fanie_bugenne@ens.univ-artois.fr>
2025-11-27 15:23:12 +0100

    front page et header.php

---
+++ wp-content/themes/revelacteur/front-page.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Template Name: Page d'Accueil Personnalisée
+ *
+ * Utilisé pour afficher la page d'accueil d'un site.
+ * A la plus haute priorité sur tout autre template d'accueil.
+ */
+
+get_header(); // Inclut le contenu de header.php
+?>
+
+<main id="primary" class="site-main">
+
+    <?php 
+    // VOS BLOCS DE CONTENU DYNAMIQUES VONT ICI
+    ?>
+
+</main><?php
+get_footer(); // Inclut le contenu de footer.php
+?>
