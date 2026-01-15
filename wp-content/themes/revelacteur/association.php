b40b02187486a0cb4cdf13424fdb52e460fee875
Fanfaneee <fanie_bugenne@ens.univ-artois.fr>
2026-01-15 14:03:44 +0100

    btn primary and secondary remade together

--- wp-content/themes/revelacteur/association.php
+++ wp-content/themes/revelacteur/association.php
@@ -11,7 +11,7 @@ get_header(); ?>
         <div class="container" ">
             
              <div style="height: 45vh;   background-color: var(--color-primary);  display: flex;   align-items: center;  position: relative;  overflow: hidden; /* Important : Coupe l'image si elle dépasse */">
-            <h1 class="page-title" style="margin: 0;  margin-left: 10vw;   text-align: left;    font-family: 'RedsAglonema', cursive;  color: var(--color-secondary); font-size: 6rem;z-index: 10; /* Important : Assure que le texte passe AU-DESSUS de l'image */position: relative; ">L'Association</h1>
+            <h1 class="page-title" style="margin: 0;  margin-left: 10vw;   text-align: left;    font-family: 'RedsAglonema', cursive;  color: var(--color-secondary); font-size: 6rem;z-index: 10; /* Important : Assure que le texte passe AU-DESSUS de l'image */position: relative; ">Qui sommes nous ?</h1>
 
             <img src="<?php echo get_template_directory_uri(); ?>/assets/img/forme_verte.svg" alt="" class="decoration-verte" style="position: absolute; right: 0; top: 10%; /* Centre l'origine de l'image verticalement */transform: translateY(-50%) rotate(-180deg); /* Centre parfaitement + ta rotation */ height: 200%; /* Ajuste la taille de la forme (essaie 80% à 100%) */width: auto; pointer-events: none; /* Empêche de cliquer sur l'image par erreur */" />
         </div>
